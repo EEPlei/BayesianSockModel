4e89977bd1b96be77464b35256428521091eb53e
Lei Qian <lei.qian@duke.edu>
2015-11-09 15:22:33 -0500

    added conditionals

--- ui.R
+++ ui.R
@@ -51,13 +51,51 @@ shinyUI(
       hr(),
       h4("Hyperparameters:"),
       conditionalPanel(
-        condition = "input.total_prior == 'pois'",
+        condition="input.total_prior == 'pois'",
         sliderInput("total_lambda",
-                    HTML("Total prior = &lamgda;"), 
+                    HTML("Total prior - &lambda;"), 
                     value = 50, 
-                    min = 1, 
-                    max = 120)
-        
+                    min=1, 
+                    max=120)
+      ),
+      conditionalPanel(
+        condition="input.total_prior == 'nbin'",
+        numericInput("total_r",
+                     HTML("Total prior - r"), 
+                     value = 50, 
+                     min=1, 
+                     max=120),
+        numericInput("total_p",
+                     HTML("Total prior - p"), 
+                     value = 0.5, 
+                     min=0, 
+                     max=1)
+      ),
+      conditionalPanel(
+        condition="input.prop_prior == 'beta'",
+        numericInput("prop_alpha",
+                     HTML("Total prior - &alpha;"), 
+                     value = 1/3 , 
+                     min=0, 
+                     max=NA),
+        numericInput("prop_beta",
+                     HTML("Total prior - &beta;"), 
+                     value = 1, 
+                     min=0, 
+                     max=NA)
+      ),
+      conditionalPanel(
+        condition="input.prop_prior == 'tnorm'",
+        numericInput("prop_mu",
+                     HTML("Proportion prior - &mu;"), 
+                     value = 0.5, 
+                     min=NA, 
+                     max=NA),
+        numericInput("prop_sigma",
+                     HTML("Proportion prior - &sigma;"), 
+                     value = 0.1, 
+                     min=0, 
+                     max=NA)
       )
     ),
     mainPanel(
