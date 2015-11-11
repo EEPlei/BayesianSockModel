8d6e980382e40c4ade5e9fffb284a111485bdfb5
Lei Qian <lei.qian@duke.edu>
2015-11-11 17:25:48 -0500

    made things more viewable

--- ui.R
+++ ui.R
@@ -9,11 +9,14 @@ shinyUI(
       "Karl Broman's Socks"
     ),
     sidebarPanel(
-      numericInput("n_sims", h4("Simulations:"),value = 1000, min = 100, step = 1),
+      numericInput("n_sims", h4("Simulations:"),
+                   value = 1000, min = 100, step = 1),
       hr(),
       h4("Data:"),
-      sliderInput("n_pairs", "Number of picked sock pairs:", min=0, max=30, value=0, step=1),
-      sliderInput("n_odd", "Number of picked odd socks:", min=0, max=30, value=11, step=1),
+      sliderInput("n_pairs", "Number of picked sock pairs:", 
+                  min=0, max=30, value=0, step=1),
+      sliderInput("n_odd", "Number of picked odd socks:", 
+                  min=0, max=30, value=11, step=1),
       hr(),
       h4("Priors:"),
       selectInput("total_prior", "Prior for total", total_priors),
@@ -22,22 +25,29 @@ shinyUI(
       h4("Hyperparameters:"),
       conditionalPanel(
         condition="input.total_prior == 'pois'",
-        sliderInput("total_lambda",HTML("Total prior - &lambda;"), value = 50, min=1, max=120)
+        sliderInput("total_lambda",HTML("Total prior - &lambda;"), 
+                    svalue = 50, min=1, max=120)
       ),
       conditionalPanel(
         condition="input.total_prior == 'nbinom'",
-        numericInput("total_r",HTML("Total prior - r"), value = 50, min=1, max=120),
-        numericInput("total_p",HTML("Total prior - p"), value = 0.5, min=0, max=1)
+        numericInput("total_r",HTML("Total prior - r"), 
+                     value = 50, min=1, max=120),
+        numericInput("total_p",HTML("Total prior - p"), 
+                     value = 0.5, min=0, max=1)
       ),
       conditionalPanel(
         condition="input.prop_prior == 'beta'",
-        numericInput("prop_alpha",HTML("Total prior - &alpha;"), value = 1/3 , min=0, max=NA),
-        numericInput("prop_beta",HTML("Total prior - &beta;"), value = 1, min=0, max=NA)
+        numericInput("prop_alpha",HTML("Total prior - &alpha;"), 
+                     value = 1/3 , min=0, max=NA),
+        numericInput("prop_beta",HTML("Total prior - &beta;"), 
+                     value = 1, min=0, max=NA)
       ),
       conditionalPanel(
         condition="input.prop_prior == 'tnorm'",
-        numericInput("prop_mu",HTML("Proportion prior - &mu;"), value = 0.5, min=NA, max=NA),
-        numericInput("prop_sigma",HTML("Proportion prior - &sigma;"), value = 0.1, min=0, max=NA)
+        numericInput("prop_mu",HTML("Proportion prior - &mu;"), 
+                     value = 0.5, min=NA, max=NA),
+        numericInput("prop_sigma",HTML("Proportion prior - &sigma;"), 
+                     value = 0.1, min=0, max=NA)
       )
     ),
     mainPanel(
